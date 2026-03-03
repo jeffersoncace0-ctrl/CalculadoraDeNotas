e6aa547c4ea3e6c251ed46f14c8cc73d017cf99b
Jheff <cacerezjefferson@gmail.com>
2026-03-03 10:31:29 -0500

    validador de correo

---
+++ ValidadorDeCorreos.py
@@ -0,0 +1,11 @@
+
+email = input ("Enter your email address please: ")
+
+if email.count("@") == 1 and "." in email.split ("@")[1]:
+                                                                  
+    print ("valid")
+
+else:
+    print ("Invalid")
+
+        
